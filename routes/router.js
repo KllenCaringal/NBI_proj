db776e45dd0837aa47078d2224841cfc67682dd1
Shieren1 <shierinemaeang2m@gmail.com>
2024-11-03 13:33:13 +0800

    routes

--- routes/router.js
+++ routes/router.js
@@ -15,7 +15,12 @@ router.get('/admin', UserCon.admin_page);
 router.get('/user', UserCon.user_page);
 router.get('/profile', UserCon.profile);
 
-
+router.get('/admindashboard', UserCon.admin_dashboard);
+router.get('/adminusers', UserCon.admin_users);
+router.get('/adminupload', UserCon.admin_upload);
+router.get('/adminnotification', UserCon.admin_notification);
+router.get('/adminrecent', UserCon.admin_recent);
+router.get('/admintrash', UserCon.admin_trash);
 
 
 module.exports = router;
